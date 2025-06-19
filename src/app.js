35cb2eb7c9561b607cb1f2aa4af887c8a861d754
itsabhay1 <abhayag41@gmail.com>
2025-06-19 20:46:12 +0530

    added deployed link in google auth callback

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8000/auth/google/callback",
+      callbackURL: "https://student-management-system-alphame.vercel.app/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
